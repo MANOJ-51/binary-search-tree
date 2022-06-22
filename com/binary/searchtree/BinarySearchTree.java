f7770908cd91322bac06aa59956990a538a56c39
MANOJ-51 <m4anoj51@gmail.com>
2022-06-22 16:17:23 +0530

    displaying welcome message

---
+++ com/binary/searchtree/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.binary.searchtree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO BINARY SEARCH TREE");
+    }
+}
