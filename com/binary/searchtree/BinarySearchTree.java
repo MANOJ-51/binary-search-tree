5db19a0a678e7405ff5f2fa59830011d5cbb290c
MANOJ-51 <m4anoj51@gmail.com>
2022-06-22 23:21:57 +0530

    added: method to find size of the binary search tree

--- com/binary/searchtree/BinarySearchTree.java
+++ com/binary/searchtree/BinarySearchTree.java
@@ -8,9 +8,21 @@ public class BinarySearchTree<M extends Comparable<M>> {
         System.out.println("WELCOME TO BINARY SEARCH TREE");
         System.out.println(" ");
         BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<Integer>();
-        binarySearchTree.add(56);    //        56
-        binarySearchTree.add(30);    //   30       \
-        binarySearchTree.add(70);    //              70
+        binarySearchTree.add(56);    //                            56
+        binarySearchTree.add(30);    //               30                     70
+        binarySearchTree.add(70);    //           22      40            60          95
+        binarySearchTree.add(22);    //       11                            65
+        binarySearchTree.add(40);    //    3      16                   63      67
+        binarySearchTree.add(11);
+        binarySearchTree.add(3);
+        binarySearchTree.add(16);
+        binarySearchTree.add(60);
+        binarySearchTree.add(95);
+        binarySearchTree.add(63);
+        binarySearchTree.add(65);
+        binarySearchTree.add(67);
+        //size
+        System.out.println("size of the binary search tree:" + binarySearchTree.size());
 
         System.out.println("In Order Traversal:- left , root ,right ");
         binarySearchTree.inOrderTraversal(root);
@@ -43,7 +55,7 @@ public class BinarySearchTree<M extends Comparable<M>> {
 
     //adding keys recursively
     public void add(M key) {
-        this.root = insertElementRecursive(root, key);
+        root = insertElementRecursive(root, key);
     }
 
     //in order traversal:- left ,root ,right
@@ -74,6 +86,19 @@ public class BinarySearchTree<M extends Comparable<M>> {
         postOrderTraversal(node.left);
         postOrderTraversal(node.right);
         System.out.println(node.key + " ");
+    }
+
+    //finding size of the root
+    public int findSize(MyBinaryNode<M> root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return findSize(root.right) + 1 + findSize(root.left);
+        }
+    }
+
+    public int size() {
+        return findSize(root);
     }
 
 }
